cf6960e924d4d0217a86e20c40b2169bba9b46d9
unknown <gizmo104567@gmail.com>
2018-02-15 21:24:45 -0600

    first commit

---
+++ Pybank/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 18:36:06 2018
+
+@author: gizmo
+"""
+
---
+++ Pypoll/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 19:16:57 2018
+hello1
+@author: gizmo
+"""
+
